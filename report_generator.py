005613a10fcf3dc8d3190210e31a49a18f4630db
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-19 15:03:03 -0700

    Create report_generator.py
    Module that generates pdf report of type and amount of fruit uploaded to catalog in specified format.

---
+++ report_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Module that generates pdf report 
+of type and amount of fruit uploaded to catalog
+"""
+
+from reportlabs.platypus import getSampleStyleSheet
+from reportlabs.platypus import SimpleDocTemplate
+from reportlabs.platypus import Paragraph, Spacer
+
+def generate_report(filename, title, info):
+  styles = getSampleStyleSheet()
+  report = SimpleDocTemplate(filename)
+  report_title = Paragraph(title, styles["h1"])
+  report_info = Paragraph(info, styles["BodyText"])
+  empty_line = Spacer(1,20)
+  report.build([report_title, empty_line, report_info, empty_line])
