498ebcc0ce270464d18fd2c67940f79119c26d7e
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-19 14:22:53 -0700

    Completed TODO: descr_dict['image_name'] value
     Completed - TODO: need to add value for image_name key that matches .jpeg to description text file
    
    Used os.path.splitext() to get name of file
    Combined file name with ".jpeg" to get value for descr_dict['image_name']

--- info_process_and_upload.py
+++ info_process_and_upload.py
@@ -24,14 +24,16 @@ for item in os.listdir(descr_dir):
     # intializes path of text file
     item_path = str(descr_dir) + str(item)
     with open(item_path) as descr:
-      # reads and splits text file by line, removes whitespace, and adds each line as a value in descr_dir
+      # splits the item name and ext (i.e. 001, 002, 003) for image_name key value in descr_dir
+      item_name, item_ext = os.path.splitext(item)
+      # reads and splits text file by line, removes whitespace, and for use as key values in descr_dir
       descr_line = descr.read().split('\n')
       descr_dict['name'] = descr_line[0].strip()
       # additionally removes 'lbs' string and converts to integer
       descr_dict['weight (in lbs)'] = int(descr_line[1].replace('lbs', '').strip())
       descr_dict['description'] = descr_line[3].strip()
       #TODO: need to add value for image_name key that matches .jpeg to description text file
-      descr_dict['image_name'] =
+      descr_dict['image_name'] = str(item_name) + ".jpeg"
       # sends a POST request to the endpoint of the web service and then prints the status code of the request
       response = requests.post(url, json=descr_dict)
       print(response.status_code)
