6303d0915efb0c716be872d32fa073d2c26b6665
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-19 15:41:22 -0700

    Create report_pdf_emailer.py
    Module for generating and sending emails

---
+++ report_pdf_emailer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import email.message
+import mimetypes
+import os.path
+import smtplib
+
+"""Module for generating and sending emails"""
+
+def generate(sender, recipient, subject, body, attachment_path):
+  """Creates an email with an attachment."""
+  # Basic email formatting
+  message = email.message.EmailMessage()
+  message["From"] = sender
+  message["To"] = recipient
+  message["Subject"] = subject
+  message.set_content(body)
+  # Processes the attachment and adds it to the email
+  attachment_filename = os.path.basename(attachment_path)
+  mime_type, _ = mimetypes.guess_type(attachment_path)
+  mime_type, mime_subtype = mime_type.split('/', 1)
+  
+  with open(attachment_path, 'rb') as ap:
+    message.add_attachment(ap.read(), maintype = mime_type, subtype = mime_subtype, filename = attachment_filename)
+  return message
+
+def send(message):
+  """Sends the message to the configured SMTP server."""
+  mail_server = smtplib.SMTP('localhost')
+  mail_server.send_message(message)
+  mail_server.quit()
