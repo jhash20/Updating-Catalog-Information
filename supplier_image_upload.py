a83426534030eba92bca989e33fd4f6ca189cdd0
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-19 13:11:10 -0700

    Create supplier_image_upload.py
    First draft of supplier_image_upload.py
    
    Script that iterates through directory of images and uploads .jpeg files to web service endpoint

---
+++ supplier_image_upload.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os, sys
+import requests
+
+"""
+Script that iterates through directory of images 
+and uploads .jpeg files to web service endpoint
+"""
+
+# initializes directory path and moves to directory
+dir_path = os.path.expand.usr('~') + '/supplier-data/images/'
+os.chdir(dir_path)
+# intializes url variable
+url = "http://localhost/upload/"
+
+# iterates through directory
+for file in os.listdir(dir_path):
+  # sorts for .jpeg files
+  if file.endwith(".jpeg", ".jpg"):
+    file_path = str(dir_path) + str(file)
+    with open(file_path, 'rb') as opened:
+      # makes a POST request to web service and then prints the response status code
+      response = request.post(url, files={'file': opened})
+      print(response, status_code)
+  else:
+    # prints an error that the script did not process file as it was not a .jpeg
+    print("Error: Did not process, " + file + " is not a .jpeg")
