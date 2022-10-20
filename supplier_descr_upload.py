23bd6027cb2d9e18e3d45b599148d81fb35ea8d2
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-20 13:05:39 -0700

    Fixed key name
    Web service endpoint was expecting the key name to be different then what was entered in script; caused 400 status code when trying to upload

--- supplier_descr_upload.py
+++ supplier_descr_upload.py
@@ -30,7 +30,7 @@ for item in os.listdir(descr_dir):
       descr_line = descr.read().split('\n')
       descr_dict['name'] = descr_line[0].strip()
       # additionally removes 'lbs' string and converts to integer
-      descr_dict['weight (in lbs)'] = int(descr_line[1].replace('lbs', '').strip())
+      descr_dict['weight'] = int(descr_line[1].replace('lbs', '').strip())
       descr_dict['description'] = descr_line[2].strip()
       # uses item_name + .jpeg value so Django application can pair description and image in catalog
       descr_dict['image_name'] = str(item_name) + ".jpeg"
