2b6011613b2360379c68b5af65f7066b783d2eb8
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-19 20:23:18 -0700

    Create health_check.py
    Script that runs checks for cpu usage, disk space usage, memory usage, and hostname, and then sends an email alert if check fails.

---
+++ health_check.py
@@ -0,0 +1,61 @@
+#!/usr/env/bin python3
+
+import os, sys
+import psutil
+import shutil
+from report_emailer import generate as email_generate
+from report_emailer import send as email_send
+
+"""
+Script that runs checks 
+for cpu usage, disk space usage, memory usage, and hostname
+and then sends an email alert if check fails
+"""
+
+def check_cpu_usage():
+  """ """
+  cpu_usage = psutil.cpu_percent(4) 
+  if int(cpu_usage) > 80:
+    case = "CPU usage is over 80%"
+    email_alert(case)
+
+def check_free_disk_space(disk):
+  """Checks if available disk space is less than 20%"""
+  disk_usage = shutil.disk_usage(disk)
+  disk_available = int(disk_usage.free/disk_usage.total) * 100
+  if disk_available < 20:
+    case = "Available disk space is less than 20%"
+    email_alert(case)
+      
+def check_available_memory():
+  """Checks if available memory is less than 500MB"""
+  memory = psutil.virutal_memory()
+  # converts memory available to MB
+  memory_available = int(memory.available) / 1024 ** 2 
+  if memory_available < 500:
+    case = "Available memory is less than 500MB"
+    email_alert(case)
+  
+def check_hostname():
+  """Checks if the hostname cannot be resolved to IP address"""
+  #TODO: function
+  case = "local host cannot be resolved"
+
+def email_alert(case):  
+  """Generates and sends an email alert with error case if any check fails"""
+  sender = "automation@example.com"
+  recipient = "{}@example.com".fomat(os.environ["USER"])
+  subject = "Error - {}".format(case)
+  body = "Please check your system and resolve the issue as soon as possible."
+  message = email_generate(sender, recipient, subject, body)
+  email_send(message)
+
+def main(argv):
+  """Runs checks for cpu usage, disk space usage, memory usage, and hostname""""
+  check_cpu_usage()
+  check_free_disk_space("/")
+  check_available_memory()
+  check_hostname()
+  
+if __name__ == "__main__":
+  main(sys.argv)
