1654e875df38664e9193c5833e4ca9b754f2bb9d
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-20 12:45:50 -0700

    Fixed typo
    Fixed typo missing s in requests
    
    Removed .jpg parameter being passed to file.endswith()

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -17,11 +17,11 @@ url = "http://localhost/upload/"
 # iterates through directory
 for file in os.listdir(dir_path):
   # sorts for .jpeg files
-  if file.endswith(".jpeg", ".jpg"):
+  if file.endswith(".jpeg"):
     file_path = str(dir_path) + str(file)
     with open(file_path, 'rb') as opened:
       # makes a POST request to web service endpoint and then prints the response status code
-      response = request.post(url, files={'file': opened})
+      response = requests.post(url, files={'file': opened})
       print(response.status_code)
   else:
     # prints an error that the script did not process file as it was not a .jpeg
