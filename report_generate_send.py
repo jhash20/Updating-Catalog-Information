c87811e9ab26a4aadb4c28ddd6f40b6c7685afb6
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-20 17:13:31 -0700

    Changed  strftime() format; changed comments
    Changed  strftime() format: Changed to correct format for report title
    
    Changed comments: removed unnecessary comments and made the current ones more useful using PEP

--- report_generate_send.py
+++ report_generate_send.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-'''
-Script that processes text files in a directory and 
+"""Processes text files in a directory and 
 generates a report and sends it to supplier via email, 
 detailing the type and amount of each fruit uploaded to catalog.
-'''
+"""
 
-# imports necessary modules
 import os, sys
 import datetime
 from report_generator import generate as pdf_generate
@@ -14,48 +12,40 @@ from report_emailer import generate as email_generate
 from report_emailer import send as email_send
 
 def generate_info(dir_path):
-  '''Generates info string with newlines in correct format for pdf BodyText.'''
-  # initalizes list to store each item string 
+  """Processes info from text files in directory into string with correct format for pdf's BodyText."""
   info_list = []
-  # iterates through directory of text files passed by main() 
+  # iterates through directory of text files, reformats into string, and adds to list
   for item in os.listdir(dir_path):
-    # conditional ensures that files iterated are text files
     if item.endswith(".txt"):
-      # initializes full item path to be opened
       item_path = str(dir_path) + str(item)
       with open(item_path) as info:
-        # splits info in item into lines and then strips whitespace/makes string
         info_line = info.read().split('\n')
         name = str(info_line[0].strip())
         weight = str(info_line[1].strip())
-        newline = '<br/>'
-        # adds specified lines and newline symbols into one string
-        newstring = 'name: ' + name + newline + 'weight: ' + weight + newline + newline
-        # appends strings to list
+        newline = "<br/>"
+        newstring = "name: " + name + newline + "weight: " + weight + newline + newline
         info_list.append(newstring)
   # joins list into one string and then returns string to main()
   info_string = ''.join(info_list)
   return info_string
 
 def main(argv):
-  '''Processes directory of text files, generates a pdf, and sends pdf via email.'''
+  """Processes directory of text files, generates a pdf, and sends pdf via email."""  
   # gets today's date in GMT time (default) to be used for title of report pdf
-  current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-  # initializes directory path to generate info to be used for info of report pdf
+  current_date = datetime.datetime.now().strftime("%B %-d, %Y")
   dir_path = os.path.expanduser('~') + "/supplier-data/descriptions/"
-  # initializes arguments to be passed to pdf_generate()
+  
+  pdf_path = "/tmp/processed.pdf"
   title = "Processed Update on " + str(current_date)
   info = generate_info(dir_path)
-  pdf_path = "/tmp/processed.pdf"
-  # generates pdf report
   pdf_generate(pdf_path, title, info)
-  # intializes arguments to be passed to email_generate()
+
   sender = "automation@example.com"
   recipient = "{}@example.com".format(os.environ["USER"])
   subject = "Upload Completed - Online Fruit Store"
   body =  "All fruits are uploaded to our website successfully. A detailed list is attached to this email"
   attachment_path = pdf_path
-  # generates and sends email with pdf attachment
+
   message = email_generate(sender, recipient, subject, body, attachment_path)
   email_send(message)
   
