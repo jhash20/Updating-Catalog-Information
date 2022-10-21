bb579a72afda3fc8253bfc44e9a87490568bcfb6
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-20 18:00:15 -0700

    Standardizing  quotation marks; cleaning comments
    Standardizing quotations marks: usage of ' vs. "
    
    Cleaning comments: removing unnecessary comments; making comments more useful

--- supplier_image_modify.py
+++ supplier_image_modify.py
@@ -3,32 +3,29 @@
 import os, sys
 from PIL import Image
 
-'''
-Script to reformat images:
+"""Script to reformat images:
 from: .TIFF format with image resolution 3000x2000
 to: .JPEG format with image resolution 600x400
-'''
+"""
 
-# changes to directory path containing images to be modified
-dir_path = os.path.expanduser('~') + '/supplier-data/images/'
+dir_path = os.path.expanduser("~") + "/supplier-data/images/"
 os.chdir(dir_path)
 
 # iterates through files in directory
 for root, dirs, files in os.walk("."):
   for file in files:
-    # ensures only .tiff files are being iterated on since .jpegs are being saved in same path
+    # ensures only .tiff files being iterated (.jpegs saved in same path)
     if file.endswith(".tiff"):
       # splits file name and ext
       f, e = os.path.splitext(file)
-      # joins directory path and file name to create new and old file paths
+      
       old_file_path = str(dir_path + file)
       new_file_path = os.path.join(dir_path, f)
-      # reformats images to specified format and image resolution and saves in the same path
+      # converts images to specifications and saves in same path
       try:
         with Image.open(old_file_path) as im:
-          new_im = im.convert('RGB')
+          new_im = im.convert("RGB")
           new_im = new_im.resize((600,400))
           new_im = new_im.save(new_file_path + ".jpeg")
-      # prints an exception if unable to convert file and file name
       except (IOError, OSError):
-        print('Cannot convert: ' + str(f))
+        print("Cannot convert: " + str(f))
