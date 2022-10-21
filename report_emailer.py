888ed0c168ae76164db343e299b58768579f5796
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-20 17:21:07 -0700

    Formatting

--- report_emailer.py
+++ report_emailer.py
@@ -14,14 +14,15 @@ def generate(sender, recipient, subject, body, attachment_path):
   message["To"] = recipient
   message["Subject"] = subject
   message.set_content(body)
+  
   # checks if there is an attachment. If so, attaches it
   if attachment_path != None:
     attachment_filename = os.path.basename(attachment_path)
     mime_type, _ = mimetypes.guess_type(attachment_path)
     mime_type, mime_subtype = mime_type.split("/", 1)
-    
     with open(attachment_path, "rb") as ap:
       message.add_attachment(ap.read(), maintype = mime_type, subtype = mime_subtype, filename = attachment_filename)
+      
   return message
 
 def send(message):
