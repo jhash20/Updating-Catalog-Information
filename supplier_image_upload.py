c40b0883db7e5dfc006d0889473c1108049d9b25
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-20 18:37:43 -0700

    Standardizing quotation mark; cleaning comments
    Standardizing quotation mark usage: "" vs ''
    
    Cleaning comments: removing unnecessary comments and making comments more useful per PEP

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -3,26 +3,23 @@
 import os, sys
 import requests
 
-"""
-Script that iterates through directory of images 
+"""Script that iterates through directory of images 
 and uploads .jpeg files to web service endpoint
+by making a POST request and then prints a response status code
 """
 
-# initializes directory path and moves to directory
 dir_path = os.path.expanduser('~') + '/supplier-data/images/'
 os.chdir(dir_path)
-# intializes url variable
+
 url = "http://localhost/upload/"
 
-# iterates through directory
+# iterates through directory of images and uploads them
 for file in os.listdir(dir_path):
-  # sorts for .jpeg files
+  # ensures that files iterated are .jpeg files
   if file.endswith(".jpeg"):
     file_path = str(dir_path) + str(file)
-    with open(file_path, 'rb') as opened:
-      # makes a POST request to web service endpoint and then prints the response status code
-      response = requests.post(url, files={'file': opened})
+    with open(file_path, "rb") as opened:
+      response = requests.post(url, files={"file": opened})
       print(response.status_code)
   else:
-    # prints an error that the script did not process file as it was not a .jpeg
     print("Error: Did not process, " + file + " is not a .jpeg")
